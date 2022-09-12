2e5861b4c2503d55cacbead225611a88cc513ead
Jalen Arrington <jmarrington178@yahoo.com>
2022-09-12 15:39:50 -0400

    Hardcoded initial data

--- weight-tracker/src/App.js
+++ weight-tracker/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 
 function App() {
+  
+  const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}])
+  
   return (
     <div>
       <h3>Hello World</h3>
