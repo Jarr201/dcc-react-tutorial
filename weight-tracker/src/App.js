ff0ab68376cc521824380f2f451f49c73e0cb7d3
Jalen Arrington <jmarrington178@yahoo.com>
2022-09-12 15:44:00 -0400

    Hardcoded data into the table

--- weight-tracker/src/App.js
+++ weight-tracker/src/App.js
@@ -11,9 +11,16 @@ function App() {
           <tr>
             <th>Entry Number</th>
             <th>Weight</th>
-            <th>Entry Number</th>
+            <th>Date</th>
           </tr>
         </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>175</td>
+            <td>11-23-2021</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
